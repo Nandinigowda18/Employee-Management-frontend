b83f241b01449fe76ba1ecd20f7f288e650e85ad
Nandinigowda18 <nandinigowda08756@gmail.com>
2026-03-18 15:48:57 +0530

    api issue reolved

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,4 +1,4 @@
-git add .import { useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import API from "../Services/Api";
 import "../Styles/Register.css";
